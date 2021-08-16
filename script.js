60f1c5a33fe74a5d90a3559b5a249a8607e86992
Ivan <ivang12@g.ucla.edu>
2021-08-16 14:24:53 -0700

    querySelect

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+// selecting elements 
+let score1 = document.getElementById('score--0');
+let score2 = document.getElementById('score--1');
+let dice = document.querySelector(".dice");
+
+let score1Text = score1.textContent;
+let score2Text = score2.textContent;
+
+score1 = 0;
+score2 = 0;
+
+dice.classList.add('hidden');
